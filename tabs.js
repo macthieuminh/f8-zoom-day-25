e51cdaee1f13b30923c5f76313b3c28ace023257
macthieuminh <macthieuminh@gmail.com>
2025-06-26 12:32:21 +0700

    hoàn thành bài tập tabs

---
+++ tabs.js
@@ -0,0 +1,29 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const tabs = $$(".tab")
+const contents = $$(".tab-content")
+
+function renderTab(indexTab) {
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+            tabs.forEach((t) => t.classList.remove("active"))
+            contents.forEach((c) => c.classList.remove("active"))
+
+            tab.classList.add("active")
+            document.getElementById(tab.dataset.tab).classList.add("active")
+        })
+    })
+}
+
+function keydownTab() {
+    document.body.addEventListener("keydown", (e) => {
+        let tab = tabs[e.key - 1]
+        tabs.forEach((t) => t.classList.remove("active"))
+        contents.forEach((c) => c.classList.remove("active"))
+        tab.classList.add("active")
+        document.getElementById(tab.dataset.tab).classList.add("active")
+    })
+}
+renderTab()
+keydownTab()
